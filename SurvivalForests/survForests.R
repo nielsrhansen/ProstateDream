65a34deaee62d3edd4147564f8c03dd3e4e7720c
Niels Richard Hansen <Niels.R.Hansen@math.ku.dk>
2016-02-12 12:57:29 +0100

    Survival forests given an argument for variable selection.

--- SurvivalForests/survForests.R
+++ SurvivalForests/survForests.R
@@ -1,23 +1,22 @@
+## Required packages
+library(randomForestSRC)
+
 # random survival forest
 # loads the a required package and defines a function that takes a test and training data set
 # and returns the prediction for the test data set.
 
+survForest <- function(train, test, varNames = NULL) {
 
-library(randomForestSRC)
-
-survForest <- function(train, test) {
-
+  if(is.null(varNames)) {
+    ## No variable selection
+    varNames <- setdiff(names(train), c("LKADT_P", "DEATH"))
+  }
+    
   rf.form <- as.formula(paste("Surv(LKADT_P, DEATH) ~", 
-                              paste(setdiff(names(train), c("LKADT_P", "DEATH")), collapse = " + "), 
+                              paste(varNames, collapse = " + "), 
                               collapse = ""))
   
   rf.o <- rfsrc(rf.form, data = train,
                 mtry = 20, nsplit = 10, nodesize = 6)
   predict(rf.o, newdata = test)$predicted
-  
 }
-
-
-
-
-  
