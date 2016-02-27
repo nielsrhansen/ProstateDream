bb5f567d09ad5724181fc148afba6ea433fefa5e
Niels Richard Hansen <Niels.R.Hansen@math.ku.dk>
2016-02-27 11:43:56 +0100

    Correlation on pairwise complete cases. Changed method for hierarchical clustering.

--- Exploratory/explorative.R
+++ Exploratory/explorative.R
@@ -1,4 +1,5 @@
 library(lattice)
+library(RColorBrewer)
 # Fraction of missing observations
 frac <- 0.5
 missings <- names(training[apply(training, 2, function(x) sum(is.na(x))) > 1600*frac]) 
@@ -11,27 +12,29 @@ twoLevels <- names(varLevels[varLevels == 2])
 # Level plot
 csubTable <- subTable[complete.cases(subTable[, twoLevels]), which(names(subTable) %in% twoLevels)]
 cp <- cor(data.matrix(csubTable), method = "spearman")
-ord <- rev(hclust(as.dist(1-abs(cp)))$order)
-colPal <- colorRampPalette(cbPalette[c(8,7)], space = "rgb")(100)
-levelplot(cp[ord[1:33],ord[1:33]], xlab = "", ylab = "", col.regions = colPal,
+ord <- rev(hclust(as.dist(1-abs(cp)), method = "centroid")$order)
+colPal <- colorRampPalette(c("purple", "yellow"), space = "rgb")(100)
+levelplot(cp[ord,ord], xlab = "", ylab = "", col.regions = colPal,
                     at = seq(-1, 1, length.out = 100), aspect="fill",
                     colorkey = list(space = "top"),
                     scales = list(x = list(rot = 90),
                                   y = list(draw = FALSE)))
 # Scatter plot matrix
-csubTable <- subTable[complete.cases(subTable[,c("LKADT_P", conVarCT)]),c("LKADT_P", conVarCT)]
-splom(csubTable, 
+# csubTable <- subTable[complete.cases(subTable[,c("LKADT_P", conVarCT)]),c("LKADT_P", conVarCT)]
+csubTable <- subTable[, c("LKADT_P", conVarCT)]
+cp <- cor(data.matrix(csubTable), use = "pairwise.complete.obs")
+ord <- rev(hclust(as.dist(1-abs(cp)), method = "average")$order)
+splom(csubTable[, ord], 
              upper.panel = panel.splom,
              pscale = 0, 
-             varname.cex = .3,
-             nbins = 15, xlab = NULL,
+             varname.cex = .6,
+             nbins = 15, xlab = NULL, lwd = 2, 
              lower.panel = function(x, y, ...) {
-               panel.fill(col = brewer.pal(9, "RdBu")[ round(cor(x, y) * 4 + 5)])
+               panel.fill(col = brewer.pal(9, "RdBu")[ round(cor(x, y, use = "pairwise.complete.obs") * 4 + 5)])
                cpl <- current.panel.limits()
-               panel.text(mean(range(x)), mean(range(y)), 
-                          paste('P:',round(cor(x, y), digits = 2),'\n', 'S:', 
-                                round(cor(x, y, method='spearman'), digits = 2)),
-                          cex = .4)
+               panel.text(mean(range(x, na.rm = TRUE)), mean(range(y, na.rm = TRUE)), 
+                          round(cor(x, y, use = "pairwise.complete.obs"), digits = 2),
+                          cex = .8)
              },
              type = c("smooth")
 )
