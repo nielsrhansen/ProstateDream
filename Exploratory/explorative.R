e385e82eef78793bb11c09c0c2452db5db4d8247
Niels Richard Hansen <Niels.R.Hansen@math.ku.dk>
2016-02-28 23:38:57 +0100

    Modifications of correlation plots. In particular the order as selected by hierachical clustering.

--- Exploratory/explorative.R
+++ Exploratory/explorative.R
@@ -1,40 +1,70 @@
 library(lattice)
 library(RColorBrewer)
+
 # Fraction of missing observations
 frac <- 0.5
 missings <- names(training[apply(training, 2, function(x) sum(is.na(x))) > 1600*frac]) 
 subTable <- subset(training, select = names(training)[!(names(training) %in% missings)])
-disVarCT <- disVarCT[disVarCT %in% names(subTable)]
-conVarCT <- conVarCT[conVarCT %in% names(subTable)]
+disVar <- disVarCT[disVarCT %in% names(subTable)][-c(1, 2, 4, 5)]
+conVar <- conVarCT[conVarCT %in% names(subTable)][-1]
 # Levels of variables
 varLevels <- sapply(training, function(x) length(unique(x)))
 twoLevels <- names(varLevels[varLevels == 2])
 # Level plot
 csubTable <- subTable[complete.cases(subTable[, twoLevels]), which(names(subTable) %in% twoLevels)]
+csubTable <- csubTable[, colnames(csubTable) %in% disVar]
 cp <- cor(data.matrix(csubTable), method = "spearman")
-ord <- rev(hclust(as.dist(1-abs(cp)), method = "centroid")$order)
-colPal <- colorRampPalette(c("purple", "yellow"), space = "rgb")(100)
-levelplot(cp[ord,ord], xlab = "", ylab = "", col.regions = colPal,
-                    at = seq(-1, 1, length.out = 100), aspect="fill",
-                    colorkey = list(space = "top"),
-                    scales = list(x = list(rot = 90),
-                                  y = list(draw = FALSE)))
+ord <- rev(hclust(as.dist(1-abs(cp)))$order)
+colPal <- col <- colorRampPalette(c("#67001F", "#B2182B", "#D6604D", 
+                                    "#F4A582", "#FDDBC7", "#FFFFFF", "#D1E5F0", "#92C5DE", 
+                                    "#4393C3", "#2166AC", "#053061"))(200)
+
+postscript(file = "corrplotDis.eps", width = 14, height = 14,
+           onefile = FALSE, horizontal = FALSE, paper = "special")
+print(
+  levelplot(cp[ord,ord], xlab = "", ylab = "", col.regions = colPal,
+            at = seq(-1, 1, length.out = 100), aspect="fill",
+            colorkey = list(space = "top"),
+            scales = list(x = list(rot = 90)))
+)
+dev.off()
+
+postscript(file = "corrplotDisSmall.eps", width = 5, height = 5,
+           onefile = FALSE, horizontal = FALSE, paper = "special")
+print(
+  levelplot(cp[ord,ord], xlab = "", ylab = "", col.regions = colPal,
+            at = seq(-1, 1, length.out = 100), aspect="fill",
+            colorkey = list(space = "top"),
+            scales = list(x = list(draw = FALSE), y = list(draw = FALSE)))
+)
+dev.off()
+
 # Scatter plot matrix
-# csubTable <- subTable[complete.cases(subTable[,c("LKADT_P", conVarCT)]),c("LKADT_P", conVarCT)]
-csubTable <- subTable[, c("LKADT_P", conVarCT)]
+csubTable <- subTable[, conVar]
 cp <- cor(data.matrix(csubTable), use = "pairwise.complete.obs")
 ord <- rev(hclust(as.dist(1-abs(cp)), method = "average")$order)
-splom(csubTable[, ord], 
-             upper.panel = panel.splom,
-             pscale = 0, 
-             varname.cex = .6,
-             nbins = 15, xlab = NULL, lwd = 2, 
-             lower.panel = function(x, y, ...) {
-               panel.fill(col = brewer.pal(9, "RdBu")[ round(cor(x, y, use = "pairwise.complete.obs") * 4 + 5)])
-               cpl <- current.panel.limits()
-               panel.text(mean(range(x, na.rm = TRUE)), mean(range(y, na.rm = TRUE)), 
-                          round(cor(x, y, use = "pairwise.complete.obs"), digits = 2),
-                          cex = .8)
-             },
-             type = c("smooth")
+varnames <- colnames(cp[ord, ord])
+varnames[c(18, 20)] <- c("WEIGHT", "HEIGHT")
+
+postscript(file = "corrplotCont.eps", width = 10, height = 10,
+           onefile = FALSE, horizontal = FALSE, paper = "special")
+print(
+  splom(csubTable[, ord], 
+        upper.panel = panel.splom,
+        pscale = 0, 
+        varname.cex = .6,
+        nbins = 15, 
+        xlab = NULL, 
+        lwd = 2, 
+        varnames = varnames,
+        lower.panel = function(x, y, ...) {
+          panel.fill(col = brewer.pal(9, "RdBu")[ round(cor(x, y, use = "pairwise.complete.obs") * 4 + 5)])
+          cpl <- current.panel.limits()
+          panel.text(mean(range(x, na.rm = TRUE)), mean(range(y, na.rm = TRUE)), 
+                     round(cor(x, y, use = "pairwise.complete.obs"), digits = 2),
+                     cex = .8)
+        },
+        type = "smooth"
+  )
 )
+dev.off()
